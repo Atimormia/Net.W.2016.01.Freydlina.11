62ffc9b50d823a3f5987a23b3ce249d0d80e154d
atimormia <atimormia@gmail.com>
2016-11-20 17:34:53 +0300

    Task1

---
+++ Net.W.2016.01.Freydlina.11/Task1/Generator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Generator
+    {
+        public static IEnumerable<int> Fibonacci(int count)
+        {
+            if (count <= 0) throw new ArgumentException("Count must be positive");
+
+            int a1 = 1, a2 = 1;
+            for(int i = 0; i < count; i++)
+            {
+                if (i == 0 || i == 1)
+                {
+                    yield return 1;
+                    continue;
+                }
+                int result = a1 + a2;
+                yield return result;
+                a1 = a2;
+                a2 = result;
+            }
+        }
+    }
+}
