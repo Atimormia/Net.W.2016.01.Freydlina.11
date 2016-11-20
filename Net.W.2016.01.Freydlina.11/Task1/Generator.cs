9949670519bd1cdac515967a6fff8f26126eb238
atimormia <atimormia@gmail.com>
2016-11-20 17:39:17 +0300

    Task1

--- Net.W.2016.01.Freydlina.11/Task1/Generator.cs
+++ Net.W.2016.01.Freydlina.11/Task1/Generator.cs
@@ -8,6 +8,12 @@ namespace Task1
 {
     public class Generator
     {
+        /// <summary>
+        /// Generates Fibonacci numbers
+        /// </summary>
+        /// <param name="count">Count of Fibonacci numbers</param>
+        /// <returns><see cref="IEnumerable{T}"/> collection</returns>
+        /// <exception cref="ArgumentException">Throws if count less than 0 or equals 0</exception>
         public static IEnumerable<int> Fibonacci(int count)
         {
             if (count <= 0) throw new ArgumentException("Count must be positive");
